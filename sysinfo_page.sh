d0b9df13c0a897ea20dcb5f2bf82d22598e8997c
victor-arana <victor_arana@hotmail.com>
2019-11-17 17:24:44 -0600

    Produce an html page

---
+++ sysinfo_page.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# sysinfo_page - A script to produce an html file
+
+echo "<html>"
+echo "<head>"
+echo "	<title>"
+echo "	The title of your page"
+echo "	</title>"
+echo "</head>"
+echo ""
+echo "<body>"
+echo " Your page content goes here."
+echo "</body>"
+echo "</html>"
